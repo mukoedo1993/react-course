2f10d468aa8e38deef0f850e4fb41bbed83aec64
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-09-12 14:54:19 -0700

    update part of our project in course 21st

---
+++ complex-react-app/app/components/Page.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react"
+import Container from './Container'
+
+
+function Page(props) {
+  useEffect(() => {
+    document.title = `${props.title}| ComplexApp`
+    window.scrollTo(0, 0) //scroll up to the top of screen
+  } , []) //The empty array shows that the function of first argument will only be run the first time component is run.
+
+  return (
+    <Container>
+    {props.children}
+    </Container>
+  )
+}
+
+export default Page
