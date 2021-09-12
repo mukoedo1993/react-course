d979ebf6aa5b1a9e53367d623432aea42785e937
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-09-12 02:35:27 -0700

    until course 20th

--- complex-react-app/app/components/Container.js
+++ complex-react-app/app/components/Container.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react"
 
-function Container() {
-  return <></>
+//props.childern is a generic box that includes all of the child contents of the Container component.
+
+function Container(props) {
+  return <div className={"container py-md-5 " + (props.wide ? "" : "container--narrow")}>{props.children}</div>
 }
 
 export default Container
+//container container--narrow py-md-5
