a308c8238d8af0ec3b5e53066659a23094b0b93b
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-09-16 01:35:59 -0700

    update course 45th

--- complex-react-app/app/components/Page.js
+++ complex-react-app/app/components/Page.js
@@ -5,7 +5,7 @@ function Page(props) {
   useEffect(() => {
     document.title = `${props.title}| ComplexApp`
     window.scrollTo(0, 0) //scroll up to the top of screen
-  }, []) //The empty array shows that the function of first argument will only be run the first time component is run.
+  }, [props.title]) //Any time props.title change, this function will be rerun.
 
   return <Container wide={props.wide}>{props.children}</Container>
 }
