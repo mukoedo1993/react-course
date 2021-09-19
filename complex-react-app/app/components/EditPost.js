ca0bc0cffcc378ccd1cb0a4a6cb98e0ae05e2065
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-09-18 22:51:29 -0700

    course 52nd

--- complex-react-app/app/components/EditPost.js
+++ complex-react-app/app/components/EditPost.js
@@ -48,13 +48,17 @@ function ViewSinglePost() {
         draft.isFetching = false
         return
       case "titleChange":
+        draft.title.hasErrors = false
         draft.title.value = action.value
         return
       case "bodyChange":
+        draft.body.hasErrors = false
         draft.body.value = action.value
         return
       case "submitRequest":
-        draft.sendCount++
+        if (!draft.title.hasErrors && !draft.body.hasErrors) {
+          draft.sendCount++
+        }
         return
       case "saveRequestStarted":
         draft.isSaving = true
@@ -62,6 +66,23 @@ function ViewSinglePost() {
       case "saveRequestFinished":
         draft.isSaving = false
         return
+      case "titleRules":
+        if (!action.value.trim()) {
+          draft.title.hasErrors = true
+          draft.title.message = "You must provide a title"
+        } else {
+          draft.title.hasErrors = false
+          draft.title.message = ""
+        }
+        return
+      case "bodyRules":
+        if (!action.value.trim()) {
+          draft.body.hasErrors = true
+          draft.body.message = "You must provide a body"
+        } else {
+          draft.body.hasErrors = false
+          draft.body.message = ""
+        }
     }
   }
 
@@ -69,6 +90,8 @@ function ViewSinglePost() {
 
   function submitHandler(e) {
     e.preventDefault()
+    dispatch({ type: "titleRules", value: state.title.value }) //to validate rules in frontend side before it is submitted.
+    dispatch({ type: "bodyRules", value: state.body.value })
     dispatch({ type: "submitRequest" })
   }
 
@@ -124,14 +147,16 @@ function ViewSinglePost() {
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input onChange={(e) => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          <input onBlur={(e) => dispatch({ type: "titleRules", value: e.target.value })} onChange={(e) => dispatch({ type: "titleChange", value: e.target.value })} value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          {state.title.hasErrors && <div className="alert alert-danger small liveValidateMessage">{state.title.message}</div>}
         </div>
 
         <div className="form-group">
           <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea onChange={(e) => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value}></textarea>
+          <textarea onBlur={(e) => dispatch({ type: "bodyRules", value: e.target.value })} onChange={(e) => dispatch({ type: "bodyChange", value: e.target.value })} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.body.value}></textarea>
+          {state.body.hasErrors && <div className="alert alert-danger small liveValidateMessage">{state.body.message}</div>}
         </div>
 
         <button className="btn btn-primary" disabled={state.isSaving}>
